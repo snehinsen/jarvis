a2cc8549307c20baa2c4fd393b89262c333c4fb1
The Lion Cub <snehinsen@gmail.com>
2025-09-28 20:27:59 -0400

    WOrk in progress updates

---
+++ Server/src/test/kotlin/jarvis/server/ServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package jarvis.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
